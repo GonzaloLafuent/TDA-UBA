4afbad68a58c7aca7f30a16f6eee7ac9e175d834
Gonzalo Lafuente <lafuentegonzalo1@gmail.com>
2024-03-31 17:07:08 -0300

    archivos entregable y entrada de datos

---
+++ Entregable-1/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <iostream>
+#include <tuple>
+
+using namespace std;
+
+int main(){
+    string name = "";
+    int attack_ability = 0;
+    int defense_ability = 0; 
+    vector<tuple<string,int,int>> players = {};
+
+    tuple<string,int,int> player;
+
+    /*
+    Ingreso de datos por consola
+    for (size_t i = 0; i < 10; i++){
+        cin>>name;
+        cin>>attack_ability;
+        cin>>defense_ability;
+
+        player = make_tuple(name,attack_ability,defense_ability);
+        players.push_back(player);
+    }
+    */
+
+
+    return 0;
+}
+
