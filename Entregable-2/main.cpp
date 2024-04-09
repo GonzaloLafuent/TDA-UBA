57bf8dfd99cf2be20d1f25cb7b5c97269e34e48f
Gonzalo Lafuente <lafuentegonzalo1@gmail.com>
2024-04-09 09:15:07 -0300

    elimino archivos que no sirven

--- Entregable-2/main.cpp
+++ Entregable-2/main.cpp
@@ -4,84 +4,91 @@
 #include <utility>
 #include <chrono>
 #include <thread>
+#include <limits>
+#include <algorithm>
 
 using namespace std;
 using namespace std::chrono;
 
-//building = (width,height)
-vector<pair<int,int>> buildings = {};
+struct building{
+    int width;
+    int heigth;
+};
+
+vector<building> buildings = {};
 
 int max_increasing = -1;
 int max_decreasing = -1;
+vector<int> mem = {};
 
-void skylines(int i,int n,bool inc,bool dec,int subseq_width,vector<pair<int,int>>& buildings_subseq){
-    if(i == n){
-        if(buildings_subseq.size()==1 || inc)
-            max_increasing = (subseq_width > max_increasing)?subseq_width: max_increasing; 
-        if(buildings_subseq.size() ==1 || dec)
-            max_decreasing = (subseq_width > max_decreasing)?subseq_width: max_decreasing;
-    } else if( buildings_subseq.size() == 1){
-        if( buildings[i].second > buildings_subseq.back().second ){
-            //Si el proximo elemento es mayor en altura, estimo que va ser creciente
-            buildings_subseq.push_back(buildings[i]);
-            skylines(i+1,n,true,false,subseq_width + buildings[i].first ,buildings_subseq);
-
-            buildings_subseq.pop_back(); 
-
-            skylines(i+1,n,false,false,subseq_width,buildings_subseq);
-        } else if( buildings[i].second < buildings_subseq.back().second){
-            //Si el proximo elemnto es menor en altura, estimo que va a ser menor
-            buildings_subseq.push_back(buildings[i]);
-            skylines(i+1,n,false,true,subseq_width + buildings[i].first ,buildings_subseq);
-            buildings_subseq.pop_back();
-
-            skylines(i+1,n,false,false,subseq_width,buildings_subseq);
-        } 
-        skylines(i+1,n,false,false,subseq_width,buildings_subseq);
-    } else if( buildings_subseq.size() > 1){
-        if(inc && buildings[i].second > buildings_subseq.back().second){
-            buildings_subseq.push_back(buildings[i]);
-            skylines(i+1,n,true,false,subseq_width + buildings[i].first ,buildings_subseq);
-            buildings_subseq.pop_back();
-        } else if( dec && buildings[i].second < buildings_subseq.back().second ) {
-            buildings_subseq.push_back(buildings[i]);
-            skylines(i+1,n,false,true,subseq_width + buildings[i].first ,buildings_subseq);
-            buildings_subseq.pop_back();
+int max_inc(int i,int n){
+    if(mem[i] ==-1){
+        if( i == n-1){
+            max_increasing = buildings[i].width; 
+            return mem[i] = buildings[i].width;
+        } else{
+            int max_width = buildings[i].width;
+            for (int j = i+1; j < n; j++){
+                //subsecuencia que contiene a elmento j
+                int sub_seq_width = max_inc(j,n);
+                if( buildings[i].heigth < buildings[j].heigth){
+                    if(max_width < buildings[i].width + sub_seq_width)
+                        max_width = buildings[i].width + sub_seq_width;
+                } 
+            }
+            max_increasing = (max_width > max_increasing)? max_width : max_increasing;
+            mem[i] = max_width; 
         }
-        skylines(i+1,n,inc,dec,subseq_width,buildings_subseq);
-    } else if(buildings_subseq.size()==0){
-        buildings_subseq.push_back(buildings[i]);
-
-        skylines(i+1,n,false,false,subseq_width + buildings[i].first ,buildings_subseq);
-
-        buildings_subseq.pop_back();
-
-        skylines(i+1,n,false,false,subseq_width,buildings_subseq);
+        return mem[i];
     }
+    return mem[i];
+}
+
+int max_dec(int i,int n){
+    if(mem[i] ==-1){
+        if( i == n-1){
+            max_decreasing = buildings[i].width;
+            return mem[i] = buildings[i].width;
+        } else{
+            int max_width = buildings[i].width;
+            for (int j = i+1; j < n; j++){
+                //subsecuencia que contiene a elmento j
+                int sub_seq_width = max_dec(j,n);
+                if( buildings[i].heigth > buildings[j].heigth){
+                    if(max_width < buildings[i].width + sub_seq_width)
+                        max_width = buildings[i].width + sub_seq_width;
+                } 
+            }
+            max_decreasing = (max_width > max_decreasing)? max_width : max_decreasing;
+            mem[i] = max_width; 
+        }
+        return mem[i];
+    }
+    return mem[i];
 }
 
 int main(){
     int t = 0;
     cin>>t;
 
-    vector<vector<pair<int,int>>> cases(t,vector<pair<int,int>>(0));
+    vector<vector<building>> cases(t,vector<building>(0));
 
     for (int i = 0; i < t; i++){
         int cant_of_buildings = 0;
         cin>>cant_of_buildings;
 
-        buildings = vector<pair<int,int>>(cant_of_buildings,{0,0});
+        buildings = vector<building>(cant_of_buildings,{0,0});
 
         for (int i = 0; i < cant_of_buildings; i++){
             int height = 0;
             cin>>height;
-            buildings[i].second = height;
+            buildings[i].heigth = height;
         }
         
         for (int i = 0; i < cant_of_buildings; i++){
             int width = 0;
             cin>>width;
-            buildings[i].first = width;
+            buildings[i].width = width;
         }
 
         cases[i] = buildings;
@@ -92,17 +99,14 @@ int main(){
         max_increasing = -1;
         max_decreasing = -1;
 
-        vector<pair<int,int>> buildings_subseq = {};
-        auto start = high_resolution_clock::now();
-        skylines(0,buildings.size(),false,false,0,buildings_subseq);
-        auto end = high_resolution_clock::now();
-        auto duration = duration_cast<microseconds>(end - start);
-
+       mem = vector<int>(buildings.size(),-1);
+       max_inc(0,buildings.size());
+       mem = vector<int>(buildings.size(),-1);
+       max_dec(0,buildings.size());
+    
         if( max_increasing >= max_decreasing )
             cout<<"Case "<<(i+1)<<". Increasing ("<<max_increasing<<"). Decreasing ("<<max_decreasing<<")."<<endl;
         else cout<<"Case "<<(i+1)<<". Decreasing ("<<max_decreasing<<"). Increasing ("<<max_increasing<<")."<<endl;
-
-        cout <<"Time: "<<duration.count() << endl;
     }
 
 }
