7d90d389fa273fdacdd040d64e36cf47d5047367
Tu Nombre <you@example.com>
2024-04-17 16:40:45 -0300

    buclea el 3

--- Entregable-3/main.cpp
+++ Entregable-3/main.cpp
@@ -8,7 +8,16 @@ int t = 0;
 int h = 0;
 
 vector<vector<int>> accorns = {
-
+    {1,0,0},
+    {0,2,1},
+    {0,1,0},
+    {0,1,0},
+    {0,0,1},
+    {0,1,1},
+    {1,0,1},
+    {0,1,1},
+    {0,0,0},
+    {1,0,0},
 };
 
 vector<vector<int>> mem(t,vector<int>(h,-1));
@@ -16,36 +25,43 @@ vector<vector<int>> mem(t,vector<int>(h,-1));
 // 0<= i < t , 0 <= j < h  
 int max_accorns(int i,int j){
     int acc = 0;
-    if( i< 0) return -1;
-    if( i == 0) return 0;
+    if( i >= h ) return 0;
+    if( j >= t) return 0;
+    if( i == h-1 && j<t) return accorns[i][j];
     else if( j == t-1 ){
-        acc = max_accorns(i-1,j);
-        for (size_t k = j-1; k >= 0; k++){
+        acc = max_accorns(i+1,j);
+        for (size_t k = j-1; k >= 0; k--){
             int sum = 0;
-            sum = max_accorns(i-f,k);
+            sum = max_accorns(i+f,k);
             acc = max(acc,sum);
         }
         return acc + accorns[i][j];
     } else if ( j == 0){
-        acc = max_accorns(i-1,j);
+        acc = max_accorns(i+1,j);
         for (int k = 1; k < t; k++){
             int sum = 0;
-            sum = max_accorns(i-f,k);
+            sum = max_accorns(i+f,k);
             acc = max(sum,acc);    
         }
         return acc + accorns[i][j];
     } else{
-        acc = max_accorns(i-1,j);
+        acc = max_accorns(i+1,j);
         for (int k = 0; k < t; k++){
             int sum = 0;
             if( k!=j )
-                sum = max_accorns(i-f,k);
+                sum = max_accorns(i+f,k);
             acc = max(acc,sum);
         }
         return acc + accorns[i][j];
     }
+    return acc;
 }
 
 int main(){
+    f = 2;
+    t = 3;
+    h = 10;
+    int max = max_accorns(0,0); 
+    //cout<<max<<endl;
     return 0;
 }
